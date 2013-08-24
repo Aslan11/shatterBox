7b947e6b8a834c1acaaf8e9761e1bcadcdeedee1
Kevin Martin <kvn.p.mrtn@gmail.com>
2013-08-23 20:41:17 -0700

    Mission Complete, Needs Polish

--- js/shatterBox.js
+++ js/shatterBox.js
@@ -1,24 +1,28 @@
 
 $(document).ready(function(){
+	var numShards = $('.shard').length;
+	
+	if(navigator.userAgent.indexOf("Firefox") != -1 ){
+		$('.shard').css({"margin":"-2.05px"});
+	}
+	
 	$('.shatterTrigger').hover(function(){
 		$('.shard').addClass('active');
 		$('.shatterCaption').addClass("visible");
-		$('.shatterHeader').addClass("visible");				
+		$('.shatterHeader').addClass("visible");
+		for (var i=1; i < numShards; i++){
+		var randNum = Math.floor((Math.random() * 7) + 3);
+		$('.shard:nth-child('+i+')').css({"transition":"background ."+randNum+"s, transform ."+randNum+"s","-webkit-transition":"background ."+randNum+"s, -webkit-transform ."+randNum+"s"});
+		console.log(i);
+		console.log(randNum);
+	
+	}
+						
 	})
 	$('.shatterTrigger').mouseout(function(){
 		$('.shard').removeClass('active');	
 		$('.shatterCaption').removeClass("visible");
 		$('.shatterHeader').removeClass("visible");		
 	})
-	
-	var numShards = $('.shard').length
-	
-	for (var i=1; i < numShards; i++){
-		var randNum = Math.floor((Math.random() * 9) + 1);
-		$('.shardContainer:nth-child('+i+')').css({"transition":"background ."+ randNum +"s, transform ."+ randNum +"s","-webkit-transition":"background ."+ randNum +"s, -webkit-transform ."+ randNum +"s"});
-		console.log(i);
-		console.log(randNum);
-	
-	}
 	
 });
